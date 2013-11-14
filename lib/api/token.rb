8f33b2113807cdddae39c9905a412ccba73d7bff
Yonah Forst <joshblour@hotmail.com>
2013-11-14 18:28:56 +0100

    shortened api token

--- lib/api/token.rb
+++ lib/api/token.rb
@@ -5,8 +5,8 @@ module Token
     
     def generate_token_for(member)
       #TODO: figure out how to set this token from the host application
-      token = "4c6075460341d7d2a6eacc24fc88b707"
-      json = {token: token, member_type: member.class.to_s, member_id: member.id, time: Time.now}.to_json
+      token = "4c6075460341d7d"
+      json = {token: token, type: member.class.to_s, id: member.id, time: Time.now.to_i}.to_json
       return MsgApi::Encryptor.encrypt(json)
     end
     
