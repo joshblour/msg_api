b00aee886db1b64cb76961628a1bc0dc3debe4b4
Yonah Forst <joshblour@hotmail.com>
2013-11-14 18:48:47 +0100

    use token from host application

--- lib/api/token.rb
+++ lib/api/token.rb
@@ -5,7 +5,8 @@ module Token
     
     def generate_token_for(member)
       #TODO: figure out how to set this token from the host application
-      token = "4c6075460341d7d"
+      
+      token = MsgApi::TOKEN #|| "489f5bd2c34d47e1"
       json = {token: token, type: member.class.to_s, id: member.id, time: Time.now.to_i}.to_json
       return MsgApi::Encryptor.encrypt(json)
     end
